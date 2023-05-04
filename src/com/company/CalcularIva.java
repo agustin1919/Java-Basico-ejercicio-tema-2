eacdf37ec1536acbd64003368dad29808c5c9b1f
FacundoS <agustinsouyaux@gmail.com>
2023-05-04 19:07:30 -0300

    corrigiendo ejercicio.

--- src/com/company/CalcularIva.java
+++ src/com/company/CalcularIva.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class CalcularIva {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("El precio es: ");
         double precio = scanner.nextInt();
-
         System.out.println(calcularIva(precio));
     }
     public static double calcularIva(double precio){
