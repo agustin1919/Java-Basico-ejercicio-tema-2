9195ccc0b4f1212668ad422e96524e034fff7323
FacundoS <agustinsouyaux@gmail.com>
2023-05-04 18:57:08 -0300

    primer y ultimo commit

---
+++ src/com/company/CalcularIva.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class CalcularIva {
+    public static void main(String[] args) {
+        // El iva es el 21%
+        System.out.println(calcularIva(10));
+        System.out.println(calcularIva(100));
+        System.out.println(calcularIva(1000));
+    }
+    public static double calcularIva(double precio){
+        double iva = 0.21;
+        double totalIva = precio * iva;
+        double precioConIva = totalIva + precio;
+
+
+        return precioConIva;
+    }
+}
